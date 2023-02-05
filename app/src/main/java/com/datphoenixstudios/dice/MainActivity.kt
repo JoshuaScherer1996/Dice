631456715266c2d4140be57617e7da7876f0373c
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-02-05 16:47:35 +0100

    changed the app from a textoutput to an image output

--- app/src/main/java/com/datphoenixstudios/dice/MainActivity.kt
+++ app/src/main/java/com/datphoenixstudios/dice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.datphoenixstudios.dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,15 +14,35 @@ class MainActivity : AppCompatActivity() {
 
         //connecting UI with code
         val rollButton: Button = findViewById(R.id.button)
-        val resultTextView: TextView = findViewById(R.id.textView)
+        val diceImage: ImageView = findViewById(R.id.iv_dice)
 
         //creating a dice
         val dice: Dice = Dice(6)
 
         rollButton.setOnClickListener {
-            //rolling the dice and outputting the result
+            //rolling the dice and displaying the fitting image
             val rolledDice = dice.roll()
-            resultTextView.text = rolledDice.toString()
+
+            when (rolledDice) {
+                1 -> {
+                    diceImage.setImageResource(R.drawable.dice_1)
+                }
+                2 -> {
+                    diceImage.setImageResource(R.drawable.dice_2)
+                }
+                3 -> {
+                    diceImage.setImageResource(R.drawable.dice_3)
+                }
+                4 -> {
+                    diceImage.setImageResource(R.drawable.dice_4)
+                }
+                5 -> {
+                    diceImage.setImageResource(R.drawable.dice_5)
+                }
+                6 -> {
+                    diceImage.setImageResource(R.drawable.dice_6)
+                }
+            }
         }
     }
 }
